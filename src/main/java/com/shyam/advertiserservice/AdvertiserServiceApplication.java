117c19c276496cc9f65ad86247ef2e2dd6f74b1f
Shyam Jain <shyamjain@Shyams-MacBook-Pro.local>
2018-06-12 11:03:14 -0400

    initial project setup

---
+++ src/main/java/com/shyam/advertiserservice/AdvertiserServiceApplication.java
@@ -0,0 +1,12 @@
+package com.shyam.advertiserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvertiserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvertiserServiceApplication.class, args);
+	}
+}
